e6881ba53850d3c3a38302f346ef28a6f6b1fe9f
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2026-04-22 15:56:38 +0700

    Fix: remove root redirect, documents resource handles root via index route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers\DocumentController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/documents');
-
 Route::resource('documents', DocumentController::class)->except(['edit', 'update']);
 Route::get('documents/{document}/download', [DocumentController::class, 'download'])->name('documents.download');
