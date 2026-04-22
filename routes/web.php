90ef8d3503b2fb1abd0b44f166385f0383b969e7
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2026-04-22 16:01:04 +0700

    Fix: add root route pointing to DocumentController index

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\DocumentController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [DocumentController::class, 'index']);
 Route::resource('documents', DocumentController::class)->except(['edit', 'update']);
 Route::get('documents/{document}/download', [DocumentController::class, 'download'])->name('documents.download');
